e2c3a48a429678cc1a9938d6218dff6efc2158d7
ChekushkinPM <x-chekushkin-pavel@yandex.ru>
2025-05-11 16:35:29 +0400

    React Typescript 2023 - 3. Props

---
+++ frontend/src/Components/CardList/CardList.tsx
@@ -0,0 +1,15 @@
+import Card from '../Card/Card'
+
+type Props = {}
+
+const CardList = () => {
+  return (
+    <div>
+        <Card/>
+        <Card/>
+        <Card/>
+    </div>
+  )
+}
+
+export default CardList
